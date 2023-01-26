8e7a8f05311daa428f3b38685dabd9181a41ad0d
Nikolay Indzhiev <nikolay.indzhiev@amusnet.com>
2023-01-26 18:55:41 +0200

    add Constants.ts

---
+++ src/Constants.ts
@@ -0,0 +1,34 @@
+export enum Suites {
+    Spades = 'S',
+    Hearts = 'H',
+    Clubs = 'C',
+    Diamonds = 'D'
+}
+
+export enum Ranks {
+    ACE = 'A',
+    N2 = '2',
+    N3 = '3',
+    N4 = '4',
+    N5 = '5',
+    N6 = '6',
+    N7 = '7',
+    N8 = '8',
+    N9 = '9',
+    N10 = '10',
+    JACK = 'J',
+    QUEEN = 'Q',
+    KING = 'K',
+}
+
+export const cardSize = {
+    w: 409,
+    h: 623
+};
+
+export const cardSheet = {
+    startX: 47.5,
+    startY: 848,
+    marginX: 49.4,
+    marginY: 37
+};
