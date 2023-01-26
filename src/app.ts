e2bf816df2cf6ecb6f1902b8687cbc763c3522b0
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2023-01-26 15:27:22 +0200

    add project files

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import { Connection } from "./Connection";
+import { engine } from "./engine";
+import * as PIXI from 'pixi.js';
+
+const initForm = document.querySelector('form');
+const initSection = document.getElementById('init');
+const gameSection = document.getElementById('game');
+
+let connection = null;
+
+const app = new PIXI.Application({
+    width: 800,
+    height: 600,
+    background: "#005000",
+});
+
+
+showBoard();
+
+document.getElementById("board").appendChild(app.view as HTMLCanvasElement);
+
+/*
+
+initForm.addEventListener('submit', async event => {
+    event.preventDefault();
+    const { nickname } = Object.fromEntries(new FormData(event.target as HTMLFormElement));
+
+    connection = new Connection(nickname as string);
+    await connection.open();
+    engine(connection);
+    showBoard();
+
+    connection.send('startGame');
+});
+
+document.getElementById('disconnect').addEventListener('click', () => {
+    connection?.disconnect();
+    showInit();
+});
+*/
+
+function showBoard() {
+    initSection.style.display = 'none';
+    gameSection.style.display = 'block';
+}
+
+function showInit() {
+    initSection.style.display = 'block';
+    gameSection.style.display = 'none';
+}
