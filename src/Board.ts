36b89f28630bf7b40d51770b372f93f82332412e
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2023-01-26 18:39:52 +0200

    fix y position of areas so they are below buttons

--- src/Board.ts
+++ src/Board.ts
@@ -18,13 +18,13 @@ export class Board extends Container {
     private addDeckArea() {
         const deck = new Graphics();
         deck.beginFill(this.cardColor);
-        deck.drawRoundedRect(10, 10, this.cardWidth, this.cardHeight, 12);
+        deck.drawRoundedRect(10, 50, this.cardWidth, this.cardHeight, 12);
         deck.endFill();
 
         const flippedPile = new Graphics();
         flippedPile.beginFill(this.cardColor);
         flippedPile.drawRoundedRect(0, 0, this.cardWidth, this.cardHeight, 12);
-        flippedPile.position.set(this.cardWidth + this.spaceBetweenCards + 10, 10)
+        flippedPile.position.set(this.cardWidth + this.spaceBetweenCards + 10, 50)
         flippedPile.endFill();
 
         const text = new Text("Press SPACE \n To Flip", {
@@ -32,6 +32,7 @@ export class Board extends Container {
             fontSize: 16,
             align: "center"
         });
+
 
         text.anchor.set(0.5);
         text.position.set(flippedPile.width / 2, flippedPile.height / 2);
@@ -48,10 +49,9 @@ export class Board extends Container {
             area.drawRoundedRect(0, 0, this.cardWidth, this.cardHeight, 12);
             area.endFill();
 
-            area.position.set((this.cardWidth * 3 + 70 + i * this.spaceBetweenCards) + (i * this.cardWidth), 10)
+            area.position.set((this.cardWidth * 3 + 70 + i * this.spaceBetweenCards) + (i * this.cardWidth), 50)
 
             this.addChild(area);
-            console.log(`Card ${i}`, area.x, area.y)
         }
     }
 
@@ -62,7 +62,7 @@ export class Board extends Container {
             area.drawRoundedRect(0, 0, this.cardWidth, this.cardHeight, 12);
             area.endFill();
 
-            area.position.set((10 + i * this.spaceBetweenCards) + (i * this.cardWidth), this.cardHeight + this.spaceBetweenCards)
+            area.position.set((10 + i * this.spaceBetweenCards) + (i * this.cardWidth), this.cardHeight + this.spaceBetweenCards + 50)
 
             this.addChild(area);
         }
