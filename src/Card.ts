46592bc5cad93f73c200044d5b9a87e5c16416dc
Nikolay Indzhiev <nikolay.indzhiev@amusnet.com>
2023-01-27 14:15:34 +0200

    update card boarder and readme

--- src/Card.ts
+++ src/Card.ts
@@ -18,7 +18,7 @@ export class Card extends PIXI.Container {
 
         // Border
         const border = new PIXI.Graphics();
-        border.lineStyle(10, 0x000000);
+        border.lineStyle(10, 0xa6ce39);
         border.beginFill(0x000000, 0);
         border.drawRoundedRect(5, 5, cardSize.w - 10, cardSize.h - 10, 28);
         border.endFill();
@@ -31,10 +31,6 @@ export class Card extends PIXI.Container {
         mask.endFill();
         this.mask = mask;
 
-        // Front
-        const frontTexture = new PIXI.Texture(cardsTexture.baseTexture, new PIXI.Rectangle(x, y, cardSize.w, cardSize.h));
-        this.front = PIXI.Sprite.from(frontTexture);
-
         // Back
         const backBackground = new PIXI.Graphics();
         backBackground.beginFill(0x07162B, 1);
@@ -43,18 +39,40 @@ export class Card extends PIXI.Container {
 
         const backLogo = PIXI.Sprite.from(logoTexture);
         backLogo.anchor.set(0.5);
-        backLogo.position.set(this.front.width / 2, this.front.height / 2);
+        backLogo.position.set(backBackground.width / 2, backBackground.height / 2);
         backLogo.scale.set(2.2);
 
         this.back.addChild(backBackground, backLogo);
         this.back.renderable = false;
 
+        // Front
+        const frontTexture = new PIXI.Texture(cardsTexture.baseTexture, new PIXI.Rectangle(x, y, cardSize.w, cardSize.h));
+        this.front = PIXI.Sprite.from(frontTexture);
+
         this.addChild(this.back, this.front, border, mask);
         this.pivot.x = this.width / 2;
         this.scale.set(0.334);
-
         this.position.set(100, 100);
 
+        this.makeFlippable();
+    }
+
+    private addBack() {
+        const backBackground = new PIXI.Graphics();
+        backBackground.beginFill(0x07162B, 1);
+        backBackground.drawRoundedRect(0, 0, cardSize.w, cardSize.h, 36);
+        backBackground.endFill();
+
+        const backLogo = PIXI.Sprite.from(logoTexture);
+        backLogo.anchor.set(0.5);
+        backLogo.position.set(backBackground.width / 2, backBackground.height / 2);
+        backLogo.scale.set(2.2);
+
+        this.back.addChild(backBackground, backLogo);
+        this.back.renderable = false;
+    }
+
+    private makeFlippable() {
         // Toggle front and back
         const flipTw = gsap.to(this, {
             pixi: { scaleX: 0 },
