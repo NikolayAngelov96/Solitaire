cc9f4e46e42c04a5ac1bf2a24df7b0a0f7886e2c
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2023-02-02 10:52:06 +0200

    fix Deck to extend PIXI.Container

--- src/Deck.ts
+++ src/Deck.ts
@@ -1,16 +1,18 @@
-import { CardArea } from "./CardArea";
+import { Container, Graphics } from "pixi.js";
 import { CardFactory } from "./CardFactory";
+import { colors } from "./Constants";
 import { GameManager } from "./GameManager";
 
-export class Deck extends CardArea {
+export class Deck extends Container {
 
     constructor(width: number, height: number, private gameManager: GameManager, private cardFactory: CardFactory) {
-        super(width, height);
+        super();
+        this.createArea(width, height);
         this.position.set(10 + width / 2, 50);
         this.interactive = true;
         this.fill();
 
-        this.on('pointertapcapture', () => {
+        this.on('pointertapcapture', (e) => {
             if (this.gameManager.cardsDealed) {
                 console.log('Flip card');
             }
@@ -28,5 +30,14 @@ export class Deck extends CardArea {
             this.addChild(card);
             this.gameManager.cards.push(card);
         }
+    }
+
+    private createArea(width: number, height: number) {
+        const area = new Graphics();
+        area.beginFill(colors.cardPlaceholder);
+        area.drawRoundedRect(0, 0, width, height, 12);
+        area.endFill();
+        this.pivot.x = width / 2;
+        this.addChild(area);
     }
 }
